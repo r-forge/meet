2af8020305393dfc1625a714fad83facd7668ca3
erola (Erola Pairo Castiñeira) <erola.pairo@gmail.com>
2012-06-26 13:59:23 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/meet@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/diff.instructions.R
+++
@@ -1,8 +0,0 @@
-diff.instructions <-function (training.set,HX,HXmax,Herror,Redundancia_corregida){	
-	
-		Rsequencia<-redundancy(HX,HXmax,Herror,finite=nrow(training.set),TRUE)
-		x<-sum(Redundancia_corregida*(abs(Rsequencia-Redundancia_corregida)))
-		    
-		return(x)
-}
-
